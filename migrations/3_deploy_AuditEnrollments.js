c8d22083178a8e64b9de3784656d957e7426810e
edangelo2 <enrique.dangelo@gmail.com>
2021-11-29 14:25:24 -0600

    fin

--- migrations/3_deploy_AuditEnrollments.js
+++ migrations/3_deploy_AuditEnrollments.js
@@ -3,6 +3,6 @@ const AuditEnrollments = artifacts.require("./AuditEnrollments.sol");
 module.exports = function (deployer) {
   deployer.deploy(AuditEnrollments)
   .then(() => AuditEnrollments.deployed())
-  .then(_instance => console.log('export const auditEnrollments = "',_instance.address+'"'));
+  .then(_instance => console.log('export const auditEnrollments = "'+_instance.address+'"'));
 };
 
